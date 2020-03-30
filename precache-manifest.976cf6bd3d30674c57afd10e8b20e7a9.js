762daf78134c8fa48b15b69b47c6187cf4618a60
Ian Fowler <ian@spidr.design>
2020-03-30 00:29:21 -0500

    Updates

---
+++ precache-manifest.976cf6bd3d30674c57afd10e8b20e7a9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e28c1620b9576ecae1bdb9e44cd176",
+    "url": "/Covid19LivingArchive/index.html"
+  },
+  {
+    "revision": "96e98112865f49b062cf",
+    "url": "/Covid19LivingArchive/static/css/2.a9d422b8.chunk.css"
+  },
+  {
+    "revision": "2f740fa23a8d9ad2cdb0",
+    "url": "/Covid19LivingArchive/static/css/main.49fb6980.chunk.css"
+  },
+  {
+    "revision": "96e98112865f49b062cf",
+    "url": "/Covid19LivingArchive/static/js/2.8c36b8a3.chunk.js"
+  },
+  {
+    "revision": "919a9f8729d2acae082f872b400ca6cb",
+    "url": "/Covid19LivingArchive/static/js/2.8c36b8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f740fa23a8d9ad2cdb0",
+    "url": "/Covid19LivingArchive/static/js/main.1331df8a.chunk.js"
+  },
+  {
+    "revision": "ca8b38fc1205f4b543d8",
+    "url": "/Covid19LivingArchive/static/js/runtime-main.4a0b28df.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/Covid19LivingArchive/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/Covid19LivingArchive/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/Covid19LivingArchive/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/Covid19LivingArchive/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/Covid19LivingArchive/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/Covid19LivingArchive/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/Covid19LivingArchive/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/Covid19LivingArchive/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/Covid19LivingArchive/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/Covid19LivingArchive/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/Covid19LivingArchive/static/media/9.565ad4a3.jpeg"
+  }
+]);
