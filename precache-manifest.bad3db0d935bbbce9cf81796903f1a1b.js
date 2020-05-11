fb9bb214373714ccf427a8b836b874e03dc8d17e
Ian Fowler <ian@spidr.design>
2020-05-11 14:35:37 -0500

    Updates

---
+++ precache-manifest.bad3db0d935bbbce9cf81796903f1a1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e731cf177e2a87b82174d4edd3c2d934",
+    "url": "/Covid19LivingArchive/index.html"
+  },
+  {
+    "revision": "ab4bdfc09098ffdf9994",
+    "url": "/Covid19LivingArchive/static/css/2.a9d422b8.chunk.css"
+  },
+  {
+    "revision": "5864843a9345db62ac49",
+    "url": "/Covid19LivingArchive/static/css/main.49fb6980.chunk.css"
+  },
+  {
+    "revision": "ab4bdfc09098ffdf9994",
+    "url": "/Covid19LivingArchive/static/js/2.832fc6e3.chunk.js"
+  },
+  {
+    "revision": "919a9f8729d2acae082f872b400ca6cb",
+    "url": "/Covid19LivingArchive/static/js/2.832fc6e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5864843a9345db62ac49",
+    "url": "/Covid19LivingArchive/static/js/main.aa35390e.chunk.js"
+  },
+  {
+    "revision": "ca8b38fc1205f4b543d8",
+    "url": "/Covid19LivingArchive/static/js/runtime-main.4a0b28df.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/Covid19LivingArchive/static/media/0.73476783.jpg"
+  }
+]);
